0300e66ac09bde84a8038a36816b074ea9d64a00
nin042 <shosundberg@gmail.com>
2016-11-17 19:01:45 +0700

    made simple foobar

--- lib/ruboty/keywords.rb
+++ lib/ruboty/keywords.rb
@@ -1,7 +1,18 @@
+require 'ruboty'
 require "ruboty/keywords/version"
 
 module Ruboty
   module Keywords
-    # Your code goes here...
+    class Response < Base
+      on(
+        /foo/,
+        name: 'foo',
+        description: 'return bar'
+      )
+
+      def response(message)
+        message.reply("bar")
+      end
+    end
   end
 end
