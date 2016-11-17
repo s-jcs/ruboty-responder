5452860231ecb4a499d405778b642935a434367e
nin042 <shosundberg@gmail.com>
2016-11-17 18:31:05 +0700

    first commit

---
+++ lib/ruboty/keywords.rb
@@ -0,0 +1,7 @@
+require "ruboty/keywords/version"
+
+module Ruboty
+  module Keywords
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/keywords/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Keywords
+    VERSION = "0.1.0"
+  end
+end
